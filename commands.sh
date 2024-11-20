b6903e9902e23f3e0dc457d0713cba6fe5890c3f
cynak <dr1197dr@gmail.com>
2024-11-20 13:59:46 -0500

    Add more smaba related fixes

--- commands.sh
+++ commands.sh
@@ -1,8 +1,11 @@
 mkdir /opt/samba-flask
+#create samba-flask group
+groupadd samba-flask
+./add_samba_to_sudo.sh
 chmod 777 /opt/samba-flask
 chown nobody:nogroup /opt/samba-flask
 git clone https://github.com/cynak/samba-web-interface.git
-chown -R samaba-flask:samba-flask /opt/samba-flask/
+chown -R samba-flask:samba-flask /opt/samba-flask/
 cd /opt/samba-flask/samba-web-interface
 conda create -n samba-flask python==3.10 --file requirements.txt
 conda activate samba-flask
